66699662b52e82de62ba0f3db2b12756ab418a0d
zradosłav <zradoslav@airmail.cc>
2018-11-22 03:31:10 +0200

    Added: - ISBN regex now handles trailing X (e.g., 123456789-X) - added termination when -type of -n were not set - satisfying some -pedantic compiler warnings

--- main.c
+++ main.c
@@ -12,30 +12,30 @@
 
 static TessBaseAPI* api;
 
-static const char* isbn_rx_str = "isbn[\\-\\s]*?(1[03])?[:\\s]*([\\-\\d\\s]+\\d)";
+static const char* isbn_rx_str = "isbn[-\\s]*?(1[03])?[:\\s]*([-\\d\\s]+[x\\d])";
 static regex_t isbn_rx;
 
 // later i'll add per-format embedded text extraction (if any)
-int extract_text_ocr_static(const image_t image, char* tbuff, size_t tbuff_size)
+char* extract_text_ocr(const image_t image, char* tbuff, size_t tbuff_size)
 {
 	TessBaseAPISetImage(api, image.data, image.width, image.height, image.bytes_per_pixel, image.bytes_per_line);
 	if(TessBaseAPIRecognize(api, NULL))
-		return -1;
+		return NULL;
 
 	char* text = TessBaseAPIGetUTF8Text(api);
 	if(!text)
-		return -1;
+		return NULL;
 
 #ifdef DEBUG
 	size_t text_len = strlen(text);
 	if(tbuff_size < text_len)
-		print_err("%s: insufficient storage: %zu < %zu\n", __FUNCTION__, tbuff_size, text_len);
+		print_err("%s: insufficient storage: %zu < %zu\n", __func__, tbuff_size, text_len);
 #endif
 
 	strncpy(tbuff, text, tbuff_size);
 	TessDeleteText(text);
 
-	return strlen(tbuff);
+	return tbuff;
 }
 
 void match_isbn(const char* text)
@@ -55,7 +55,7 @@ void match_isbn(const char* text)
 		for(int i = 0; i < max_groups; i++)
 		{
 			// empty group, just skip
-			if(groups[i].rm_so == (size_t)(-1))
+			if(groups[i].rm_so == -1)
 				continue;
 
 			// group 0 indicates whole match
@@ -68,7 +68,7 @@ void match_isbn(const char* text)
 			strncpy(match_str, cursor + groups[i].rm_so, match_len);
 			match_str[match_len] = '\0';
 
-			print_log("%s: match %u, group %u: [%u-%u]: %s\n", __FUNCTION__,
+			print_log("%s: match %u, group %u: [%u-%u]: %s\n", __func__,
 			          matches, i, groups[i].rm_so, groups[i].rm_eo, match_str);
 
 			free(match_str);
@@ -93,7 +93,7 @@ static void help()
 {
 	const char* opts = "tnflh";
 	printf("\n");
-	for(size_t i = 0; i < strlen(opts); i++)
+	for(int i = 0; i < strlen(opts); i++)
 		printf(" -%c\t%s\n", opts[i], opt_desc[opts[i]]);
 	printf("\n");
 }
@@ -117,6 +117,7 @@ int main(int argc, char* argv[])
 			type = optarg;
 			break;
 		case 'n':
+			/* here '-1' can be used to read the whole file (?) */
 			page_count = atoi(optarg);
 			break;
 		case 'f':
@@ -135,6 +136,12 @@ int main(int argc, char* argv[])
 			retcode = EXIT_FAILURE;
 			goto exit;
 		}
+	}
+	if(!type || !page_count)
+	{
+		fprintf(stderr, "Invalid arguments\n");
+		retcode = EXIT_FAILURE;
+		goto exit;
 	}
 	if(!file)
 	{
@@ -194,7 +201,7 @@ int main(int argc, char* argv[])
 	for(int i = 0; i < page_count; i++)
 	{
 		char text[2048] = { 0 };
-		extract_text_ocr_static(images[i], text, sizeof(text));
+		extract_text_ocr(images[i], text, sizeof(text));
 
 		match_isbn(text);
 	}
